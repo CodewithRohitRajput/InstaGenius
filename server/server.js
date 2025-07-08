a8b9009f1370cdc1f680587e7958d9d73be6e33c
Rohit singh rajput <rohitsinghrajput2k24@gmail.com>
2025-07-09 01:17:52 +0530

    Update server.js
    added missing

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bot = require('./gemini/bot')
 
 app.use(cors())
 app.use(express.json())
-app.use(cors({origin : 'https://instagenius-seven.vercel.app/'})
+app.use(cors({origin : 'https://instagenius-seven.vercel.app/'}))
 app.use('/' , bot)
 
 app.listen(port , ()=>{
