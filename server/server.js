1f1468339306be6f1b87be5debce13a79b05b3c3
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2025-07-09 15:25:11 +0530

    added cron job

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const cors = require('cors')
 const bot = require('./gemini/bot')
+const cron = require('node-cron')
+const fetch = require('node-fetch')
 
 app.use(cors())
 app.use(express.json())
@@ -14,6 +16,11 @@ app.use('/' , bot)
 
 app.listen(port , ()=>{
     console.log("Server is running on port 3000")
+})
+
+
+cron.schedule('*/5 * * * *' , async ()=>{
+    const res = await fetch('https://instagenius.onrender.com')
 })
 
 
