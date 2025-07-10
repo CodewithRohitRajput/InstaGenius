d23f0e72d472c23fefe24f135d4a64defa0bbbf8
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2025-07-10 15:21:32 +0530

    added advanced ui with toggle dark mode

--- client/app/layout.js
+++ client/app/layout.js
@@ -27,7 +27,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        < Footer/>
+        {/* < Footer/> */}
         
       </body>
     </html>
