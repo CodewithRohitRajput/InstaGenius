20d81b68ad678e495afc6f32d774e59982792a0a
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2025-07-12 13:11:27 +0530

    added image link fixed

--- client/app/layout.js
+++ client/app/layout.js
@@ -45,6 +45,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
+      <link rel="icon" href="https://www.instagenius.xyz/icon_1.png" />
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8809333825398998"
      crossorigin="anonymous"></script>
     </head>
